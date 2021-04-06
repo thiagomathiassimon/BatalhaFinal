44d765be6c422d0d25f535ef6e930d589e9b1740
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-06 16:30:01 -0300

    Exercicio_13

---
+++ src/br/com/devinhouse/batalhafinal/exceptions/IllegalMotivationSelectionException.java
@@ -0,0 +1,8 @@
+package br.com.devinhouse.batalhafinal.exceptions;
+
+public class IllegalMotivationSelectionException extends Exception{
+
+    public IllegalMotivationSelectionException(String message){
+        super(message);
+    }
+}
