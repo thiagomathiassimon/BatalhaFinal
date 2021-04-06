45c27a00214ef6908126c1f2a20465639d9d164a
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-06 10:11:45 -0300

    Ecercicio_8

---
+++ src/br/com/devinhouse/batalhafinal/interfaces/Atacante.java
@@ -0,0 +1,9 @@
+package br.com.devinhouse.batalhafinal.interfaces;
+
+import br.com.devinhouse.batalhafinal.model.Personagem;
+
+public interface Atacante {
+
+    void atacar(Personagem persongameAtacado);
+
+}
